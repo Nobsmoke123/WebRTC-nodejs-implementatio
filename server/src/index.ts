d179db45f1c883d29515625ef114f2b1cdeb2cd3
Nobsmoke123 <donaldakobundu@gmail.com>
2025-02-23 13:26:02 +0100

    Set up Express server with Socket.io and dotenv configuration

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello, world!");
+import express from "express";
+import http from "node:http";
+import * as dotenv from "dotenv";
+import { Server } from "socket.io";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+const httpServer = http.createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket);
+  console.log("a user connected");
+});
+
+httpServer.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
